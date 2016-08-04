495b98a41f8d4045d61edf22f4b9aef4660ac69c
Dan DeBrito <ddebrito@gmail.com>
2016-08-03 23:08:58 -0700

    Put all class packages into one module ControlLogix.pm

--- ControLogix.pm
+++ ControLogix.pm
@@ -1,11 +1,206 @@
 #!/usr/bin/perl
 
-package ControlLogixTag;
+package ControlLogix;
 @ISA = ();
 our $AUTOLOAD;
 
 use strict;
 use Carp qw(cluck);
+
+sub new{
+   my ($class, %args) = @_;
+
+   my %hash;
+
+   # Populate the arguments into the hash.
+   foreach my $arg (keys %args) {
+      $hash{$arg} = $args{$arg};
+   }
+
+   # DO NOT DO WORK IN THE CONSTRUCTOR!
+   #   Do work in methods/subroutines so they can be tested.
+
+   # build class data structure
+   my $self = \%hash;
+
+   bless $self, $class;
+   $self->load_session_array();
+   return $self;
+}
+
+
+sub load_session_array {
+   my $self = shift;
+   
+   my @session;
+   $session[0] = 0x65;  # register session (2)
+   $session[1] = 0x00;
+   $session[2] = 0x04; # length in bytes of data portion (2)
+   $session[3] = 0x00;
+   $session[4] = 0x00; # session id (4)
+   $session[5] = 0x00;
+   $session[6] = 0x00;
+   $session[7] = 0x00;
+   $session[8] = 0x00; # status (4)
+   $session[9] = 0x00;
+   $session[10] = 0x00;
+   $session[11] = 0x00;
+   $session[12] = 0x00; # sender context (8 - array of octet)
+   $session[13] = 0x00;
+   $session[14] = 0x00;
+   $session[15] = 0x00;
+   $session[16] = 0x00;
+   $session[17] = 0x00;
+   $session[18] = 0x00;
+   $session[19] = 0x00;
+   $session[20] = 0x00; # options flags (4)
+   $session[21] = 0x00;
+   $session[22] = 0x00;
+   $session[23] = 0x00;
+   $session[24] = 0x01; # encapsulated data array
+   $session[25] = 0x00;
+   $session[26] = 0x00;
+   $session[27] = 0x00;
+
+   $self->{session} = \@session;
+}
+
+sub tag{
+   my $self = shift;
+   my $args = shift;
+
+   if (exists $args->{name} && exists $args->{type}) {
+      sleep 0;
+   }
+
+   my $obj = ControlLogixTag->new(
+                parent => $self,
+                name   => $args->{name}, 
+                type   => $args->{type},
+   );
+
+   return $obj;
+}
+
+
+sub read_string_tag{
+   # This method is a simple interface for reading of PLC String Tags
+   my $self = shift;
+   my $tag = shift;   # Name of tag
+
+   # Note a String Tag is just a tag structure of:
+   #    .LEN   --> a DINT indicating number of characters in the string
+   #    .DATA  --> array of SINTs (up to 82) of characters
+   #
+   my $string_LEN_tag = $tag .'.LEN';
+   my $string_length_obj = $self->tag(
+                     {
+                        name => $string_LEN_tag,
+                        type => 'DINT',
+                     }
+   );
+   my $string_length = $string_length_obj->read();
+ 
+   my $string_DATA_tag = $tag .'.DATA';
+   my $string_data_obj = $self->tag(
+                     {
+                        name => $string_DATA_tag,
+                        type => 'SINT',
+                     }
+   );
+   my @sints = $string_data_obj->read($string_length);
+   my @chars = map(chr, @sints);
+   my $string = join '',@chars;
+   return $string;
+}
+
+sub write_string_tag{
+   # This method is a simple interface for reading of PLC String Tags
+   my $self = shift;
+   my $tag = shift;   # Name of tag
+   my $string = shift;
+
+   # Note a String Tag is just a tag structure of:
+   #    .LEN   --> a DINT indicating number of characters in the string
+   #    .DATA  --> array of SINTs (up to 82) of characters
+   #
+   my $length = length $string;
+   my $string_LEN_tag = $tag .'.LEN';
+   my $string_length_obj = $self->tag(
+                     {
+                        name => $string_LEN_tag,
+                        type => 'DINT',
+                     }
+   );
+   $string_length_obj->write($length);
+ 
+   my $string_DATA_tag = $tag .'.DATA';
+   my $string_data_obj = $self->tag(
+                     {
+                        name => $string_DATA_tag,
+                        type => 'SINT',
+                     }
+   );
+   my @chars = split '', $string;
+   my @sints = map(ord, @chars);
+   $string_data_obj->write(\@sints);
+}
+
+
+
+sub Log{
+   my $self = shift;
+   my $msg = shift;
+
+   my $program_name = $0;
+   if (exists $self->{log_routine}) {
+      my $log_routine = $self->{log_routine};
+      no strict;
+      &log_routine($msg);
+   }
+   elsif (open my $F, ">>$program_name.log") {
+      print $F scalar localtime;
+      print $F " $msg";
+      if ($msg =! m/\n$/) {
+         print $F "\n";
+      }
+      close $F;
+   }
+}
+
+sub AUTOLOAD{
+   my $self = shift;    
+   my $value = shift;
+
+   # This a autoload method catches any called method
+   # that is not defined. 
+
+   print "AUTOLOAD called for '$self' '$value'\n";   
+
+}
+
+sub Error{
+   my $msg = shift;
+
+   if ($msg !~ m/\n$/) {
+      $msg .= "\n";
+   }
+   # print STDERR $msg;
+   cluck $msg;
+}
+
+
+sub DESTROY{
+
+   my $self = shift;
+
+   # Do some clean just before this object is destroyed
+
+
+}
+
+
+package ControlLogixTag;
 use IO::Socket::INET;
 
 sub new{
@@ -179,16 +374,32 @@ sub get_service_request_array{
    $message_request_size += $bytes_in_path;
 
    if ($self->{op_type} eq 'write') {
-      if ($self->{type} eq 'DINT') {
-         push @arr, 0xc4; # DINT Data Type Reporting Value
-         push @arr, 0x00;
-      }
-      elsif ($self->{type} eq 'BOOL') {
-         push @arr, 0xc1; # BOOL Data Type Reporting Value
+      if ($self->{type} eq 'BOOL') {
+         push @arr, 0xC1; # BOOL Data Type Reporting Value
          push @arr, 0x00;
       }
       elsif ($self->{type} eq 'SINT') {
-         push @arr, 0xc2; # SINT Data Type Reporting Value
+         push @arr, 0xC2; # SINT Data Type Reporting Value
+         push @arr, 0x00;
+      }
+      elsif ($self->{type} eq 'INT') {
+         push @arr, 0xC3; # INT Data Type Reporting Value
+         push @arr, 0x00;
+      }
+      elsif ($self->{type} eq 'DINT') {
+         push @arr, 0xC4; # DINT Data Type Reporting Value
+         push @arr, 0x00;
+      }
+      elsif ($self->{type} eq 'REAL') {
+         push @arr, 0xCA; # REAL Data Type Reporting Value
+         push @arr, 0x00;
+      }
+      elsif ($self->{type} eq 'DWORD') {
+         push @arr, 0xD3; # DWORD Data Type Reporting Value
+         push @arr, 0x00;
+      }
+      elsif ($self->{type} eq 'LINT') {
+         push @arr, 0xc5; # LINT Data Type Reporting Value
          push @arr, 0x00;
       }
       $message_request_size += 2;
@@ -303,30 +514,46 @@ sub num_to_dint{
    }
 }
 
-sub num_to_sint{
+sub num_to_real{
+   # Convert a perl scalar number to AB ControlLogix REAL format
+   # 4 bytes long. LSB
+   # eg decimal 16 should convert to 00 00 80 41
+   # (where 41800000 is 16)
+
    my $self = shift;
    my $num = shift;
-   
-   my $return = '';
-   my $temp = $num;
-   $return = chr($temp % 256);
-   for (my $i=1; $i<4; $i++) {
-      # Fill in 0s for unused bytes.
-      $return .= chr(0);
+   my $single_precision_floating_point = pack("f",$num);
+   my @chars = split '', $single_precision_floating_point;
+   my @return;
+   for (my $i=0; $i<=3; $i++) {
+   #   my $hex = unpack "H*", $chars[$i];
+   #   $return[$i] = $hex;
+      my $ord = ord($chars[$i]);
+      $return[$i] = $ord;
    }
-   return $return;
+   return \@return;
+}
+
+
+sub real_to_num{
+   my $self = shift;
+   my @REAL = @_;
+   
+   # eg ControlLogix REAL  00 00 80 41 should convert to decimal 16
+   # (where single precision float 41800000 is 16 decimal)
+   my $string = '';
+   foreach my $b  (reverse @REAL) {
+      $string = pack("H*",$b) . $string;
+   }
+   my $num = unpack("f",$string); 
+   return $num;
 }
 
 sub sint_to_num{
-   # Convert a scalar representing Allen Bradley SINT (two bytes) into a scalar number.
+   # Convert a scalar representing Allen Bradley SINT (one byte) into a scalar number.
    my $self = shift;
    my $SINT = shift;
-   my $num = '';
-   for (my $i=0; $i<2; $i++) {
-      my $chr = substr($SINT, $i, 1);
-      my $part_num = ord($chr);
-      $num += (256 ** $i) * $part_num;
-   }
+   my $num = ord($SINT); 
    return $num;
 }
 
@@ -411,9 +638,22 @@ sub read{
 			   }
          }
       }
+      elsif ($self->{type} eq 'REAL') {
+         for (my $i=0; $i<$num_of_elements; $i++){
+            my $index = $data_start_index + ($i * 4);
+            my @arr;
+            $arr[0] = sprintf "%02x", ord($data[$index]); 
+            $arr[1] = sprintf "%02x", ord($data[$index+1]);
+            $arr[2] = sprintf "%02x", ord($data[$index+2]);
+            $arr[3] = sprintf "%02x", ord($data[$index+3]);
+            
+            my $num = $self->real_to_num( @arr );
+            $return[$i] = $num;
+         }
+      }
       else {
          my $type = $self->{type};
-         print STDERR "Undefine data type '$type' specified\n";
+         print STDERR "Undefined data type '$type' specified\n";
       }
    }
    # $DB::single = 1;
@@ -436,16 +676,28 @@ sub write{
    if (ref($data) eq 'ARRAY') {
       $self->{num_of_elements} = scalar @$data; 
       for (my $i=0; $i< scalar @$data; $i++) {
+         if ($self->{type} eq 'BOOL') {
+            push @{$self->{write_data}}, $data->[$i];
+         }
          if ($self->{type} eq 'SINT') {
             push @{$self->{write_data}}, $data->[$i];
          }
+         elsif($self->{type} eq 'DINT') {
+            my $val_str = $self->num_to_dint($data->[$i]);
+            foreach my $c (split //, $val_str) {
+               push @{$self->{write_data}}, ord($c)
+            }
+         }
+         elsif($self->{type} eq 'REAL') {
+            push @{$self->{write_data}}, @{$self->num_to_real($data->[$i])};
+         }
       }
-	  if ((scalar @$data) % 2) {
+	  if ((scalar @{$self->{write_data}}) % 2) {
 		# odd amount of data, so pad to make even
 		push @{$self->{write_data}}, 0x00;
 	  }
    }
-   else {
+   else {   # Not array
       if ($self->{type} eq 'BOOL') {
          if ($data) {
             push @{$self->{write_data}}, 0xff;
@@ -466,6 +718,9 @@ sub write{
          foreach my $c (split //, $val_str) {
             push @{$self->{write_data}}, ord($c)
          }
+      }
+      elsif ($self->{type} eq 'REAL') {
+         push @{$self->{write_data}}, @{$self->num_to_real($data)};
       }
    }
   
@@ -509,62 +764,6 @@ sub write{
    }
 }
 
-sub Log{
-   my $self = shift;
-   my $msg = shift;
-
-   my $program_name = $0;
-   if (exists $self->{log_routine}) {
-      my $log_routine = $self->{log_routine};
-      no strict;
-      &log_routine($msg);
-   }
-   elsif (open my $F, ">>$program_name.log") {
-      print $F scalar localtime;
-      print $F " $msg";
-      if ($msg =! m/\n$/) {
-         print $F "\n";
-      }
-      close $F;
-   }
-}
-
-sub AUTOLOAD{
-   my $self = shift;    
-   my $value = shift;
-
-   # This a autoload method catches any called method
-   # that is not defined. By default the set values
-
-   
-
-   my $method = $AUTOLOAD;
-   $self->Error("AUTOLOAD called for unspecified method: " . $method);
-   my $return;
-
-   return $return; 
-}
-
-sub Error{
-   my $self = shift;
-   my $msg = shift;
-
-   if ($msg !~ m/\n$/) {
-      $msg .= "\n";
-   }
-   # print STDERR $msg;
-   cluck $msg;
-}
-
-
-sub DESTROY{
-
-   my $self = shift;
-
-   # Do some clean just before this object is destroyed
-
-
-}
 
 return 1;
 
@@ -572,79 +771,91 @@ __END__
 
 =head1 NAME
 
-ControlLogixTab - this module is used by ControlLogix object
-to set up and handle CIP read/write operations 
-to/from Allen Bradley ControlLogix PLCs via TCP / ethernet.
+ControlLogix - Read/Write from/to Allen Bradley PLCs via TCP / ethernet.
 
 =head1 VERSION
 
-This documentation refers to ControlLogix and ControlLogixTag version 0.0.2.
-This worked is based on many sources. Including:
-Logix5000 Data Access ( http://literature.rockwellautomation.com/idc/groups/literature/documents/pm/1756-pm020_-en-p.pdf ) CIP Addressing Examples
-
-=head1 Acknowledgments
-   Thanks bro for your node.js initial implemantation.
-   Wireshark is great for debugging.
+This documentation refers to ControlLogix version 0.0.1.
 
 =head1 SYNOPSIS
 
-   use ControlLogic;
+   use ControlLogix;
    
    my $obj = ControlLogix->new(
       plc_ip_addr => '192.168.0.150';
       my_ip_addr => '192.168.0.100', # optional
    );
 
-   # Register a Tag. 
-   my $counter_tag = obj->tag(   # tag method returns an object of type ControlLogixTag
+   # Read/Writ to a PLC DINT tag. 
+   my $counter_tag = obj->tag(
       name => 'Counter',
       type => 'DINT',
    );
-
    my $count = $counter_tag->read();
-   $counter_tag->write(100);
+   $counter_tag->write(100);      # Set it to 100
+
+   # Read/Write a STRING from a PLC string array
+	my $tag = 'TestString[2]';
+   my $string = $plc->read_string_tag($tag);
+   print "String read test for $tag result is: '$string'\n";
+   $plc->write_string_tag($tag,'Another two');
+   $string = $plc->read_string_tag($tag);
+   print "String read test for $tag result is: '$string'\n";
+
+   # Read a ten SINT values from a PLC SINT array
+   $tag_name = 'TempSTRING.DATA';
+   my $sint_arr = $plc->tag(
+                     {
+                        name => $tag_name,
+                        type => 'SINT',
+                     }
+   );
+   my @data = $sint_arr->read(10);
+   print $tag_name . " = '@data'\n";
 
 
 =head1 DESCRIPTION 
-   This module, ControlLogixTag, is typically just used
-   by a parent object of type ControlLogix.
-
+	This module allows reading and writing to Allen Bradley ControlLogix PLCs via
+	the Common Industry Protocol (CIP -- previously Control and Information Protocol).
+	Note only works on controller tags.
+	
+	
 =head1 SUBROUTINES/METHODS
-   new - creates a tag object for reading and writing.
 
-   read - method that reads data from PLC tag.
+   new  --create object for communicating with a specific PLC
+		my $plc = ControlLogix->new(
+			plc_ip_addr => '192.0.0.200',
+		);
+  
 
-   write - method that writes data to PLC tag.
-      
+   read_string_tag - easier way to read a string tag 
+      rather than using low level queries to tag members.
+		my $string = $plc->read_string_tag('TempSTRING');
+		  
+   tag - creates a tag object for reading and writing
+		my $dint = $plc->tag(
+            {
+                name => 'test_DINT',
+                type => 'DINT',
+            }
+        );
+      my $data = $dint->read();     # Read one DINT from a DINT or DINT array tag.
+		my @data = $dint->read(4);    # Read four DINTs from a DINT array tag.
+      $dint->write(42);             # Write 42 to DINT tag called 'test_DINT'. 
+   
+   write_string_tag - easier way to write a string tag 
+      rather than using low level queries to tag members.
+		my $string = $plc->write_string_tag('TempSTRING','Killroy was here');
+   
+   
 =head1 DEPENDENCIES
+
    Carp
-   IO::Socket::INET
+   ContolLogixTag.pm
 
 =head1 AUTHOR
 
 <Dan DeBrito> (<ddebrito@gmail.com>)
-   
-   
-=head1 USAGE
-
-   use ControlLogic;
-   
-   my $obj = ControlLogix->new(
-      plc_ip_addr => '192.168.0.150';
-      my_ip_addr => '192.168.0.100', # optional
-   );
-
-   # Register a Tag. 
-   my $counter_tag = obj->tag(   # tag method returns an object of type ControlLogixTag
-      name => 'RejectCounter',
-      type => 'DINT',
-   );
-
-   my $count = $counter_tag->read();
-   $counter_tag->write(100);
-
-
-
 =head1 COPYRIGHT
 
 Copyright (c) 2015-2016 by Dan DeBrito. All rights reserved.
@@ -667,3 +878,65 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the "GNU General Public License" for more details.
 
 
+=head1 LICENCE AND COPYRIGHT
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See <perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+   
+=head1 USAGE
+
+   use ControlLogix;
+   
+   my $obj = ControlLogix->new(
+      plc_ip_addr => '192.168.0.150';
+      my_ip_addr => '192.168.0.100', # optional
+   );
+
+   # Read/Writ to a PLC DINT tag. 
+   my $counter_tag = obj->tag(
+      name => 'Counter',
+      type => 'DINT',
+   );
+   my $count = $counter_tag->read();
+   $counter_tag->write(100);      # Set it to 100
+
+   # Read/Write a STRING from a PLC string array
+	my $tag = 'TestString[2]';
+   my $string = $plc->read_string_tag($tag);
+   print "String read test for $tag result is: '$string'\n";
+   $plc->write_string_tag($tag,'Another two');
+   $string = $plc->read_string_tag($tag);
+   print "String read test for $tag result is: '$string'\n";
+
+   # Read a ten SINT values from a PLC SINT array
+   $tag_name = 'TempSTRING.DATA';
+   my $sint_arr = $plc->tag(
+                     {
+                        name => $tag_name,
+                        type => 'SINT',
+                     }
+   );
+   my @data = $sint_arr->read(10);
+   print $tag_name . " = '@data'\n";
+
+   
+   my $real_arr_tag = $plc->tag(
+                     {
+                        name => 'test_REAL_arr[1]',
+                        type => 'REAL',
+                     }
+   );
+   $real_arr_tag->write(16);
+   my $data = $real_arr_tag->read();  # data is 16
+   print "REAL data=$data\n";
+   $real_arr_tag->write(42.42);
+   my $data = $real_arr_tag->read();  # data is 42.42
+   $real_arr_tag->write(3.1412);
+   my $data = $real_arr_tag->read();  # data is 3.1412
+   my @arr = (3.1412, 42.42, 3.3333);
+   $real_arr_tag->write(\@arr);       # Write three numbers into a REAL array
+   my @nums;
+   @nums = $real_arr_tag->read(3);    # Read three numbers back out of REAL array.
